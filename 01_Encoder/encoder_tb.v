569d462ce86688ebdea0c10037222174b744406d
ishitavaid6 <172894974+ishitavaid6@users.noreply.github.com>
2026-07-16 13:49:05 +0530

    Create encoder_tb.v

---
+++ 01_Encoder/encoder_tb.v
@@ -0,0 +1,31 @@
+module stimulus;
+
+reg  [7:1] in;
+wire [3:1] out;
+
+
+encoder DUT (
+    .in(in),
+    .out(out)
+);
+
+initial begin
+    $display("Time\tInput\t\tOutput");
+    $monitor("%0t\t%b\t%b", $time, in, out);
+
+   
+    in = 7'b0000000;
+    #10;
+    in = 7'b0000001;   
+    #10;
+    in = 7'b0010000;   
+    #10;
+    in = 7'b1000000;   
+    #10;
+    in = 7'b1010000; 
+    #10;
+
+    $finish;
+end
+
+endmodule
