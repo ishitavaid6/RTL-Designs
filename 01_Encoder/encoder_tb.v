947a9929daec30a57279a80dfcfe9fadd86bc5a2
ishitavaid6 <172894974+ishitavaid6@users.noreply.github.com>
2026-07-16 13:55:50 +0530

    Update encoder_tb.v

--- 01_Encoder/encoder_tb.v
+++ 01_Encoder/encoder_tb.v
@@ -1,4 +1,5 @@
 `timescale 1ns/1ps
+
 module stimulus;
 
 reg  [7:1] in;
@@ -17,16 +18,31 @@ initial begin
    
     in = 7'b0000000;
     #10;
-    in = 7'b0000001;   
+    in = 7'b0000001;  
     #10;
-    in = 7'b0010000;   
+
+    in = 7'b0000010; 
     #10;
-    in = 7'b1000000;   
+
+    in = 7'b0000100; 
     #10;
-    in = 7'b1010000; 
+
+    in = 7'b0001000;  
+    #10;
+
+    in = 7'b0010000; 
+    #10;
+
+    in = 7'b0100000; 
+    #10;
+
+    in = 7'b1000000;
+    #10;
+    in = 7'b1010000;  
     #10;
 
     $finish;
 end
 
+endmodule
 endmodule
